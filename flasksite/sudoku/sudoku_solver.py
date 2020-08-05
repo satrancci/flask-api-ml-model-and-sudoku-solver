71150c2ec5fcfcd8507bc7dc098b5f05796cdd17
satrancci <oleksandr.ipatov@slu.edu>
2020-08-04 21:03:30 -0500

    fixed bug in solve() method and added util functions for preprocessing string input

--- flasksite/sudoku/sudoku_solver.py
+++ flasksite/sudoku/sudoku_solver.py
@@ -1,3 +1,15 @@
+######## Util functions
+
+def preprocess_sudoku(s): # s is a string of length 81; 0 stands for empty cell
+    digits = list(map(lambda x: '.' if x == '0' else x, s))
+    matrix = [digits[i:i+9] for i in range(0,len(digits),9)]
+    return matrix
+
+def postprocess_sudoku(matrix): # once solved, convert the board back into a string of length 81
+    return ''.join([matrix[i][j] for i in range(9) for j in range(9)]) 
+
+
+#########################################################
 
 class Sudoku:
 
@@ -5,7 +17,7 @@ class Sudoku:
 
     def __init__(self, matrix):
         self._matrix = matrix
-    
+      
     def is_valid(self, x, y, num):
         for i in range(self.BOARD_SIZE):
             if self._matrix[x][i] == num or self._matrix[i][y] == num or self._matrix[3*(x//3)+i//3][3*(y//3)+i%3] == num:
@@ -24,6 +36,4 @@ class Sudoku:
                             self._matrix[i][j] = '.' # otherwise, we set the current cell to empty (backtrack)
                         if num == 9: # if we tried all numbers for this cell and still didn't solve it,
                             return False # then there is no solution and we need to backtrack to make changes in earlier cells
-        return True # we are out of boundaries which means that we placed numbers on all cells and that the configuration is legal
-
-
+        return False if any(self._matrix[i][j] == '.' for i in range(self.BOARD_SIZE) for j in range(self.BOARD_SIZE)) else True #return True if we are out of boundaries which means that we placed numbers on all cells and that the configuration is legal
